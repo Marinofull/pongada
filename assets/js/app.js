2697c5230872140a9c76d9e073e973f2df4efba8
Marinofull <intmarinoreturn0@gmail.com>
2016-03-06 21:56:55 -0300

    finalizado jogo. Não tem IA (cuidado, contém altas referências memisticas)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,28 @@
 var app=angular.module('pongada', []);
 
-app.controller("game", ['$scope', function ($scope){
+app.service("gameStates", ["$http", function($http){
+
+    this.getWorld = function(){
+        return $http.get("http://localhost/pongada/docs/file.json");
+    }
+
+    this.getEnds = function(){
+        return $http.get("http://localhost/pongada/docs/ends.json");
+    }
+
+}])
+
+app.controller("game", ['$scope',"gameStates", function ($scope, gameStates){
+
+    // console.log(gameStates.getWorld());
+
+
 
     $scope.initialize = function(){
+        gameStates.getEnds().then(function(response){
+            $scope.ends = response.data.ends;
+        });
+        $scope.stopgGame = false;
         $scope.pieces=[];
         $scope.pieceAux = {inc: [0,3], img: ["gokuavatar","cellavatar"]};
         $scope.arena = [];
@@ -19,7 +39,8 @@ app.controller("game", ['$scope', function ($scope){
             $scope.node.push({
                 id: i,
                 class: ['p'+(i+1)],
-                filled: false
+                filled: false,
+                who: 0
             });
         };
 
@@ -31,6 +52,7 @@ app.controller("game", ['$scope', function ($scope){
             });
 
             $scope.node[i-1 + (jump*3)].filled = true;
+            $scope.node[i-1 + (jump*3)].who = (i < 4)  + 2 * (i >=4); // byte hack boolean extreme bind conversion
         };
 
     };
@@ -70,29 +92,59 @@ app.controller("game", ['$scope', function ($scope){
     $scope.goto = function(n){
         if (n.class[1] == "adjacence"){
             $scope.node[$scope.selected.class[0][1]-1].filled=false;
+            $scope.node[$scope.selected.class[0][1]-1].who = 0;
+            $scope.node[n.id].who = $scope.turn+1;
             $scope.selected.class[0] = 'p'+ (n.id+1);
             n.filled = true;
-            for (var i = 0; i < 9; i++)
+
+            for (var i = 0; i < $scope.node.length; i++)
                 $scope.node[i].class[1]="";
 
             $scope.selected = null;
-             $scope.turn = !$scope.turn;// de 2
-             // $scope.turn = false;
+
+            // $scope.turn = false
+            var config = "";
+            for(var k = 0; k < $scope.node.length; k++){
+                if($scope.node[k].who == $scope.turn+1)
+                    config += "1";
+                else
+                    config += "0";
+            }
+
+            var initialConfigs= ["111000000", "000000111"];
+            var playerName = ["Kakaroto","Cell"];
+            for(var k = 0; k < $scope.ends.length; k++){
+                if($scope.ends[k] == config ){
+                    if (config != initialConfigs[$scope.turn+1-1]){
+                        $scope.stopgGame = true;
+                        alert(playerName[$scope.turn+1-1] +" GANHOU O JOGO!");
+                        setTimeout($scope.initialize,9001); // EH MAIS 8 MIIIILL, CORRAM PRAS COLINAS! ALFACE
+                    }
+                    else{
+                        alert("Ladies and Gentlemen, a miss universo é " + playerName[$scope.turn+1-1] + "!");
+                        alert("Não Não desculpa, o joga ainda não acabou!");
+                    }
+                }
+            }
+            console.log(config);
+            $scope.turn = !$scope.turn;// de 2
+
+
             // cellplaying();
         };
 
-        var a = '{';
-        for(var i=0; i < 9; i++){
-            if($scope.node[i].filled){
-               for(var j = 0; j < 6 && ($scope.pieces[j].class[0][1] != (i + 1)); j++);
-               if(j < 3) a = a + '1';
-               else a = a + '2';
-            } else
-                a = a + 0;
-        }
-        a =a + ($scope.turn+1);
-        a = a + '}';
-        console.log(a);
+        // var a = '{';
+        // for(var i=0; i < 9; i++){
+        //     if($scope.node[i].filled){
+        //        for(var j = 0; j < 6 && ($scope.pieces[j].class[0][1] != (i + 1)); j++);
+        //        if(j < 3) a = a + '1';
+        //        else a = a + '2';
+        //     } else
+        //         a = a + 0;
+        // }
+        // a =a + ($scope.turn+1);
+        // a = a + '}';
+        // console.log(a);
 
 
 
@@ -102,6 +154,7 @@ app.controller("game", ['$scope', function ($scope){
         if(p.id < 4 && $scope.turn == true) return; //vez de cell clicando em goku
         if(p.id >= 4 && $scope.turn == false) return; //vez de goku clicando em cell
         if($scope.selected != null) return; //esta no meio de uma jogada
+        if($scope.stopgGame) return;
 
         $scope.selected = p;
         var adjs = adj(p.class[0][1]-1);
